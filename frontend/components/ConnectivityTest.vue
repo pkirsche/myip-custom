dea2bc3acd60f4e913fefbeea1729d17057709a7
Patrick <45640378+pkirsche@users.noreply.github.com>
2026-04-20 21:45:16 +0200

    Update ConnectivityTest.vue
    Change netcup.de to heise.de for conntest.

--- frontend/components/ConnectivityTest.vue
+++ frontend/components/ConnectivityTest.vue
@@ -86,7 +86,7 @@ const intervalId = ref(3000);
 const connectivityTests = reactive([
   { id: 'hetzner',    name: 'Hetzner',    icon: Store,              url: 'https://www.hetzner.com/favicon.ico?',                       status: t('connectivity.StatusWait'), time: 0, mintime: 0 },
   { id: 'ionos',      name: 'IONOS',      icon: Compass,            url: 'https://www.ionos.de/favicon.ico?',                          status: t('connectivity.StatusWait'), time: 0, mintime: 0 },
-  { id: 'netcup',     name: 'Netcup',     icon: Cloud,              url: 'https://www.netcup.de/favicon.ico?',                         status: t('connectivity.StatusWait'), time: 0, mintime: 0 },
+  { id: 'heise',      name: 'Heise',      icon: Cloud,              url: 'https://www.heise.de/favicon.ico?',                          status: t('connectivity.StatusWait'), time: 0, mintime: 0 },
   { id: 'google',     name: 'Google',     icon: Chrome,             url: 'https://www.google.com/favicon.ico?',                        status: t('connectivity.StatusWait'), time: 0, mintime: 0 },
   { id: 'cloudflare', name: 'Cloudflare', icon: Cloud,              url: 'https://www.cloudflare.com/favicon.ico?',                    status: t('connectivity.StatusWait'), time: 0, mintime: 0 },
   { id: 'youtube',    name: 'YouTube',    icon: Youtube,            url: 'https://www.youtube.com/favicon.ico?',                       status: t('connectivity.StatusWait'), time: 0, mintime: 0 },
